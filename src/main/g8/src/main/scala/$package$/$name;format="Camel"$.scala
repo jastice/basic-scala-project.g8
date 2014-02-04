ece0c84036a53b0ef581deae94e2f4624b0f2343
Justin Kaeser <jast@zno.de>
2014-02-04 01:01:15 +0100

    Fix errors with trailing arg

--- src/main/g8/src/main/scala/$package$/$name;format="Camel"$.scala
+++ src/main/g8/src/main/scala/$package$/$name;format="Camel"$.scala
@@ -7,7 +7,7 @@ object $name;format="Camel"$ extends App with Logging {
 
   object Args extends ScallopConf(args) {
     val param = opt[String]("param", default=Some("default"), descr="This is an example parameter")
-    val trailing = trailArg[List[String]]()
+    val trailing = trailArg[List[String]]("trailing", required=false, default=Some(Nil), descr="An optional trailing argument.")
   }
 
   logger.info(s"The parameter was: \${Args.param()}, trailing args: \${Args.trailing()}")
